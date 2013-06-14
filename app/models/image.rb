f273dfda527a1940e631abc699e44ab46f474e5c
Seliverstov Maxim <maxim.web.deveoper@gmail.com>
2013-06-15 00:31:17 +0400

    Add uploader to Image model.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ActiveRecord::Base
-  attr_accessible :description, :ip, :name, :tags, :user_agent
+  attr_accessible :description, :ip, :name, :tags, :user_agent, :file
   
+  mount_uploader :file, ImageUploader
+
 end
